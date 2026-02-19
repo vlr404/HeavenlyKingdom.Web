69abdd67da47d9f2751c8a383516c0328530d289
hellomumu <koleabumaghin@gmail.com>
2026-02-19 17:28:29 +0200

    дополнена секция Events

--- heavenlykingdom.client/src/widgets/Events/Events.jsx
+++ heavenlykingdom.client/src/widgets/Events/Events.jsx
@@ -2,13 +2,15 @@
 import { Quote } from '../../shared/Quote/Quote.jsx';
 import { Divider } from '../../shared/Divider/Divider.jsx';
 import { Calendar } from '../../shared/Calendar/Calendar.jsx';
+import { QuoteBlock } from '../../shared/QuoteBlock/QuoteBlock.jsx';
 
 export const Events = () => {
     return (
         <section id="Events" className="Events">
             <Divider title="События" />
-            <Quote text="будь зрав." />
-            <Calendar/>
+            <Quote text="Валера лох." />
+            <Calendar />
+            <QuoteBlock title="ОНЛАЙН-ПОЖЕРТВОВАНИE" quote="бла-бла-бла валера лох." backgr_img="./public/foto/pieta.png" buttonText = "Узнать больше"/>
         </section>
     );
 };
